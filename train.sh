1201b36c08a918ad2560a46aa04c8c68aa49c0de
ti <tiwang.cs@gmail.com>
2025-09-22 18:07:30 +0200

    Add training script for Gaussian model with specified parameters

---
+++ train.sh
@@ -0,0 +1,3 @@
+python main.py --train --gpu 0 --model model_Gaussian --layers 3 --channel 512 --nepoch 40 --batch_size 128
+
+# python train.py --train --gpu 0 --model model_Gaussian --layers 5 --channel 512 --nepoch 40 --batch_size 256
